39b275c030705f57d70ac596afccc29d469080cf
Johnny Peter <johnnypeter2002@gmail.com>
2018-01-28 14:57:44 +0530

    removed Unnecessary import

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
